1e74b0f47db8239860240abf6a3b7838b84f0310
Your Name <you@example.com>
2024-07-28 01:06:09 -0400

    Module 5-2

--- concert/views.py
+++ concert/views.py
@@ -38,12 +38,12 @@ def index(request):
 
 
 def songs(request):
-    songs = req.get("SONGS_URL/song").json()
+    songs = req.get("https://songs-sn-labs-myt1907.labs-prod-openshift-san-a45631dc5778dc6371c67d206ba9ae5c-0000.us-east.containers.appdomain.cloud/song").json()
     return render(request, "songs.html", {"songs":songs["songs"]})
 
 
 def photos(request):
-    photos = req.get("PHOTO_URL/picture").json()
+    photos = req.get("https://pictures.1jw6yvxji7iq.us-south.codeengine.appdomain.cloud/picture").json()
     return render(request, "photos.html", {"photos": photos})
 
 
