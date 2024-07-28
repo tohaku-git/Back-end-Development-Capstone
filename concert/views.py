918571830238679f6a97de5938519b80607e7ccc
Your Name <you@example.com>
2024-07-28 00:40:26 -0400

    Module 5

--- concert/views.py
+++ concert/views.py
@@ -38,19 +38,12 @@ def index(request):
 
 
 def songs(request):
-    songs = {"songs":[{"id":1,"title":"duis faucibus accumsan odio curabitur convallis","lyrics":"Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis."}]}
+    songs = req.get("SONGS_URL/song").json()
     return render(request, "songs.html", {"songs":songs["songs"]})
 
 
 def photos(request):
-    photos = [{
-        "id": 1,
-        "pic_url": "http://dummyimage.com/136x100.png/5fa2dd/ffffff",
-        "event_country": "United States",
-        "event_state": "District of Columbia",
-        "event_city": "Washington",
-        "event_date": "11/16/2022"
-    }]
+    photos = req.get("PHOTO_URL/picture").json()
     return render(request, "photos.html", {"photos": photos})
 
 
